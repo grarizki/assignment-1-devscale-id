49bfba92238f51d0eb622cdb5b54de2798716740
Raka Grarizki <28901232+grarizki@users.noreply.github.com>
2026-02-13 15:22:31 +0700

    Add database_url to Settings class (#2)

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "stockOptions"
     VERSION: str = "0.0.1"
+    database_url = "sqlite:///./database.db"
 
 # Create a singleton instance
 settings = Settings()
